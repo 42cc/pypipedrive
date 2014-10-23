f23f190f73ea776599aef695f8b3d60730d8d234
Alex Vykalyuk <alekzvik@gmail.com>
2014-10-23 19:29:10 +0300

    Updated version after logging changes

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pypipedrive',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     requires=['python (>= 2.7)', 'requests'],
     install_requires=['requests'],
